11192df0e71df4ac088a39079fd76cabf03f3b5f
gichul <gichul0317@gmail.com>
2021-07-08 17:29:16 -0400

    working on mobile first design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 AOS.init();
+
+const navBtn = document.querySelector('.nav-btn');
+navBtn.addEventListener('click', function () {
+  const target = document.querySelector('.menu');
+  target.classList.toggle('hidden');
+});
