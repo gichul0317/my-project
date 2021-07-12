9a5918b5e7f20f8f0195416241f5b4e4d12ec146
gichul <gichul0317@gmail.com>
2021-07-11 20:11:41 -0400

    fixed css and working on js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 'use strict';
 
-AOS.init();
+const app = {};
 
-const navBtn = document.querySelector('.nav-btn');
-navBtn.addEventListener('click', function () {
+app.displayNav = function () {
+  const navBtn = document.querySelector('.nav-btn');
   const target = document.querySelector('.menu');
-  target.classList.toggle('hidden');
-});
+  navBtn.addEventListener('click', function () {
+    target.classList.toggle('hidden');
+  });
+};
+
+app.displayLoader = function () {
+  const body = document.querySelector('body');
+  body.addEventListener('onload', function () {
+    setTimeout(app.displayPage, 2000);
+  });
+};
+
+app.displayPage = function () {
+  const page = document.querySelector('.page-container');
+  const loader = document.querySelector('.loader-container');
+  page.classList.remove('hidden');
+  loader.classList.add('hidden');
+};
+
+app.init = function () {
+  AOS.init();
+  app.displayNav();
+  app.displayLoader();
+};
+
+app.init();
