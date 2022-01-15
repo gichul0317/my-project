8388a23b1ca39a26d3d2ed7c9e02c6610142506e
Gichul Jaun <gichul@Gichuls-MacBook-Pro.local>
2022-01-15 18:21:05 -0500

    home content opacity change updated

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 
 const app = {};
 
+// make navbar color dark when scroll down
 app.navScroll = function () {
   const navbar = document.querySelector('#navbar');
   const navbarHeight = navbar.getBoundingClientRect().height;
   // console.log(navbarHeight);
   document.addEventListener('scroll', () => {
+    // console.log(window.scrollY);
     if (window.scrollY > navbarHeight) {
       navbar.classList.add('navbar--dark');
     } else {
@@ -15,11 +17,13 @@ app.navScroll = function () {
   })
 }
 
+// selector function
 app.scrollSelector = function (item) {
   const scrollTo = document.querySelector(item);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
 
+// scroll into each sections when navbar is clicked
 app.scrollPage = function () {
   const navbarMenu = document.querySelector('.navbar__menu');
   navbarMenu.addEventListener('click', (e) => {
@@ -32,10 +36,22 @@ app.scrollPage = function () {
   })
 }
 
+// scroll into contact section when home button is clicked 
 app.scrollContact = function () {
   const btn = document.querySelector('.home__button');
   btn.addEventListener('click', () => {
     app.scrollSelector('#contact');
+  })
+}
+
+// change home section's content opacity when scroll down
+app.homeTransparent = function () {
+  const home = document.querySelector('.home__container');
+  const homeHeight = home.getBoundingClientRect().height;
+  document.addEventListener('scroll', () => {
+    // console.log(homeHeight);
+    // console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = (1 - window.scrollY / homeHeight);
   })
 }
 
@@ -45,6 +61,7 @@ app.init = function () {
   app.navScroll();
   app.scrollPage();
   app.scrollContact();
+  app.homeTransparent();
 };
 
 app.init();
