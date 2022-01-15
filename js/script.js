e5bd74d5c0f14e6e68636defe7072f4279d2feef
Gichul Jaun <gichul@Gichuls-MacBook-Pro.local>
2022-01-15 17:37:17 -0500

    navbar fixed status updated

--- js/script.js
+++ js/script.js
@@ -2,50 +2,51 @@
 
 const app = {};
 
-// window.onload = app.displayLoader;
+app.navScroll = function () {
+  const navbar = document.querySelector('#navbar');
+  const navbarHeight = navbar.getBoundingClientRect().height;
+  // console.log(navbarHeight);
+  document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+      navbar.classList.add('navbar--dark');
+    } else {
+      navbar.classList.remove('navbar--dark');
+    }
+  })
+}
+
 
 app.displayNav = function () {
-  const navBtn = document.querySelector('.nav-btn');
-  const target = document.querySelector('.menu');
-  const overlay = document.querySelector('.overlay');
+  // const navBtn = document.querySelector('.nav-btn');
+  // const target = document.querySelector('.menu');
+  // const overlay = document.querySelector('.overlay');
   // navBtn.addEventListener('click', function () {
   // target.classList.toggle('hidden');
   // overlay.classList.toggle('hidden');
   // });
 };
 
-app.closeByOverlay = function () {
-  const overlay = document.querySelector('.overlay');
-  const target = document.querySelector('.menu');
-  overlay.addEventListener('click', function () {
-    // console.log('clicked');
-    overlay.classList.toggle('hidden');
-    target.classList.toggle('hidden');
-  });
-};
-
 app.displayPage = function () {
-  const page = document.querySelector('.page-container');
-  const loader = document.querySelector('.loader-container');
-  page.classList.remove('hidden');
-  loader.classList.add('hidden');
+
 };
 
 app.scrollPage = function () {
-  const btnScroll = document.querySelector('.home__scroll-down');
-  const sectionTo = document.querySelector('#about');
+  // const homeBtn = document.querySelector('');
 
-  btnScroll.addEventListener('click', function () {
-    console.log('clicked')
-    sectionTo.scrollIntoView({ behavior: 'smooth' });
-  })
+  // const btnScroll = document.querySelector('.home__scroll-down');
+  // const sectionTo = document.querySelector('#about');
+
+  // btnScroll.addEventListener('click', function () {
+  //   console.log('clicked')
+  //   sectionTo.scrollIntoView({ behavior: 'smooth' });
+  // })
 }
 
 app.init = function () {
   // AOS.init({ disable: 'mobile' });
+  app.navScroll();
   AOS.init();
   app.displayNav();
-  app.closeByOverlay();
   app.scrollPage();
 };
 
