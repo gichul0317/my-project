8006dfc006e19e90f3bb415636dfb964d5900c97
gichul <gichul0317@gmail.com>
2022-04-08 22:05:02 -0400

    js updated

--- js/script.js
+++ js/script.js
@@ -7,31 +7,44 @@ document.addEventListener('DOMContentLoaded', function () {
       perPage: 1,
       rewind: true,
       drag: 'free',
-      // autoplay: 'true',
-      // interval: 2500,
+      autoplay: 'true',
+      interval: 2000,
       pauseOnFocus: 'true',
     });
     splide.mount();
   }
 });
 
-console.log('loaded...');
-
 const toggleBtn = document.querySelector('.navbar__togglebtn');
+const menu = document.querySelector('.navbar__menu');
+const overlay = document.querySelector('.overlay');
+
 toggleBtn.addEventListener('click', () => {
-  // console.log('click');
-  const menu = document.querySelector('.navbar__menu');
   menu.classList.toggle('show');
+  overlay.classList.toggle('show');
 });
 
-//   const sectionId = ['#home', '#about', '#skills', '#work', '#contact'];
-//   const sections = sectionId.map((id) => document.querySelector(id));
-//   const navItems = sectionId.map((id) =>
-//     document.querySelector(`[data-link="${id}"]`)
-//   );
-//   // console.log(navItems);
-//   this.intersection.navItems = navItems;
-//   this.intersection.sectionId = sectionId;
+overlay.addEventListener('click', () => {
+  menu.classList.toggle('show');
+  overlay.classList.toggle('show');
+});
+
+(function () {
+  const currentPage = window.location.pathname.split('/')[1];
+  const navItems = document.querySelectorAll('.navbar__item');
+  // console.log(navItems);
+  // console.log(currentPage);
+  if (currentPage === '') return;
+  for (let i = 0; i < navItems.length; i++) {
+    // console.log(navItems[i].getAttribute('href').indexOf(currentPage));
+    if (navItems[i].getAttribute('href').indexOf(currentPage) !== -1) {
+      navItems[i].className += ' navbar__active';
+    }
+  }
+})();
+
+// this.intersection.navItems = navItems;
+// this.intersection.sectionId = sectionId;
 
 //   const selectNavItem = function (selected) {
 //     selectedNavItem.classList.remove('navbar__active');
